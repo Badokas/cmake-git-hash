fac6a291f20ae03ca4617cbae37c48ab867fe422
Mindaugas <m@v9.lt>
2025-02-21 17:38:21 +0200

    update example & workflow

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -2,12 +2,13 @@
 
 #include "GitHash.hpp"
 
-int main() {
-    std::cout << "Branch:     " << GitHash::branch << '\n';
-    std::cout << "Sha1:       " << GitHash::sha1 << '\n';
-    std::cout << "Short sha1: " << GitHash::shortSha1 << '\n';
-    std::cout << "Dirty?      " << GitHash::dirty << '\n';
-    std::cout << "Combined:   " << GitHash::shortSha1 << (GitHash::dirty ? "-dirty" : "") << '\n';
+int main()
+{
+  std::cout << "Branch:     " << GitHash::branch << '\n';
+  std::cout << "Sha1:       " << GitHash::sha1 << '\n';
+  std::cout << "Short sha1: " << GitHash::shortSha1 << '\n';
+  std::cout << "Tags:       " << GitHash::tags << '\n';
+  std::cout << "Dirty?      " << GitHash::dirty << '\n';
 
-    return 0;
+  return 0;
 }
